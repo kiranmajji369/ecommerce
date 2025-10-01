65b6bff8df046647022821493a88719134d1df37
Kiran Majji <kiranmajji369@gmail.com>
2025-10-02 03:34:03 +0530

    backend connect

--- server/index.js
+++ server/index.js
@@ -1,29 +1,53 @@
 const express = require("express");
-const mysql = require("mysql2/promise"); // promise version for async/await
+const mysql = require("mysql2/promise");
 const app = express();
+const path = require("path")
+const cors = require("cors");
 const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
   database: "ecommerce",
 });
-app.use(express.json());
+
 app.get("/categories", async (req, res) => {
   try {
-    const [rows] = await db.query("SELECT * FROM categories");
+    const [rows] = await db.query(`SELECT 
+    c.id AS category_id,
+    c.name AS category_name,
+    c.description,
+    pi.image_url AS category_image
+FROM categories c
+LEFT JOIN products p 
+    ON p.category_id = c.id
+LEFT JOIN product_images pi 
+    ON pi.product_id = p.id AND pi.is_primary = 1
+GROUP BY c.id;`);
     res.status(200).json({ data: rows });
   } catch (err) {
     console.error("Error fetching categories:", err);
     res.status(500).json({ message: "Server Error" });
   }
 });
-app.get("/products", (req, res) => {
-  res.json([
-    { id: 1, name: "Laptop", price: 50000 },
-    { id: 2, name: "Mobile", price: 20000 },
-  ]);
+
+app.get("/products", async (req, res) => {
+  try {
+    const [rows] = await db.query(`SELECT * FROM products`);
+    res.status(200).json({ data: rows });
+  } catch (err) {
+    console.error("❌ Error fetching products:", err);
+    res.status(500).json({ message: "Server Error", error: err.message });
+  }
 });
+
 app.listen(PORT, () => {
   console.log(`✅ Server started on http://localhost:${PORT}`);
 });
